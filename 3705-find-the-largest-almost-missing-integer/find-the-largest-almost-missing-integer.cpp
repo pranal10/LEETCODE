24abb7cf662ae0c7bf24f9316dd250dc1418ab14
PRANAL BHATNAGAR <pranalbhatnagar3@gmail.com>
2026-08-18 21:15:20 +0530

    Time: 5 ms (40.60%) | Memory: 32.6 MB (19.17%) - LeetSync

---
+++ 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int largestInteger(vector<int>& nums, int k) {
+        unordered_map<int, int> count;
+        int n = nums.size();
+
+        for (int i = 0; i <= n - k; i++) {
+            unordered_set<int> seen;
+
+            for (int j = i; j < i + k; j++) {
+                seen.insert(nums[j]);
+            }
+
+            // Count this subarray only once for each number
+            for (int x : seen) {
+                count[x]++;
+            }
+        }
+
+        int ans = -1;
+
+        for (auto [x, freq] : count) {
+            if (freq == 1) {
+                ans = max(ans, x);
+            }
+        }
+
+        return ans;
+    }
+};
